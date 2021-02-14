64e9468a485f831516a5b14d0aa61d7bb678ea1a
Lauri Hyvärinen <lauri.hyvarinen1@gmail.com>
2021-02-14 19:27:04 +0200

    Done? Probably needs some fine tuning

--- js/4x4.js
+++ js/4x4.js
@@ -11,7 +11,6 @@ window.onscroll = () => {
     }
 }
 
-
 function scrollToTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
